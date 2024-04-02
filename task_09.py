371ca1cdceee3b4e552779f1eda4af867d3d4b79
vprivalova <privalovaa.vv@gmail.com>
2024-04-03 04:23:17 +0700

    commit 7

---
+++ task_09.py
@@ -0,0 +1,22 @@
+class StrandsDNA:
+    all_strands = []
+
+    @staticmethod
+    def add_strands(strands):
+        StrandsDNA.all_strands.extend(strands.split())
+
+    @staticmethod
+    def get_max_strands():
+        mx_length = 0
+        result = []
+        for elem in StrandsDNA.all_strands:
+            if len(elem) > mx_length:
+                mx_length = len(elem)
+
+        for elem in StrandsDNA.all_strands:
+            if len(elem) == mx_length:
+                if elem not in result:
+                    result.append(elem)
+        result.sort()
+        return ' '.join(result)
+
